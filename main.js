ecefffa8036f32e80db0a7e4fc5a81bfbb74d075
alvin <alvinkwong1@gmail.com>
2020-09-12 04:14:34 -0400

    refactored code

--- main.js
+++ main.js
@@ -4,59 +4,39 @@ const numberOfSheetsText = document.getElementById("number-of-sheets");
 const costPerSheetText = document.getElementById("cost-per-sheet");
 const calculateButton = document.getElementById("calculate-button");
 
-const finalCost = "Cost per sheet: ";
+const finalCostPrice = document.querySelector(".final-cost__price");
+
 let price;
 let numberOfRolls;
 let numberOfSheetsPerRoll;
 let totalNumberOfSheets;
 let costPerSheet;
 
+priceText.addEventListener("input", () => {
+  price = priceText.value;
+  calculateCost();
+});
 
-// const CalculateCost = function(){
+numberOfRollsText.addEventListener("input", () => {
+  numberOfRolls = numberOfRollsText.value;
+  calculateCost();
+});
 
-//     this.getValues = function(){
-//         price = priceText.value;
-//         numberOfRolls = numberOfRollsText.value;
-//         numberOfSheetsPerRoll = numberOfSheetsText.value;
-//     }
+numberOfSheetsText.addEventListener("input", () => {
+  numberOfSheetsPerRoll = numberOfSheetsText.value;
+  calculateCost();
+});
 
-//     this.calculate = function(){
-//         totalNumberOfSheets = numberOfSheetsPerRoll * numberOfRolls;
+const calculateCost = () => {
+  totalNumberOfSheets = numberOfSheetsPerRoll * numberOfRolls;
 
-//         costPerSheet = price/totalNumberOfSheets;
-//         costPerSheetText.innerHTML = finalCost.bold() + costPerSheet.toFixed(4).toString();
-//     }
+  costPerSheet = price / totalNumberOfSheets;
 
-// }
-
-// const NewCalculateCost = new CalculateCost();
-
-// calculateButton.onclick = function(){
-//     NewCalculateCost.getValues();
-//     NewCalculateCost.calculate();
-// }
-
-
-
-
-const CalculateCost = function(){
-
-        price = priceText.value;
-        numberOfRolls = numberOfRollsText.value;
-        numberOfSheetsPerRoll = numberOfSheetsText.value;
-    
-        totalNumberOfSheets = numberOfSheetsPerRoll * numberOfRolls;
-
-        costPerSheet = price/totalNumberOfSheets;
-        
-        if (totalNumberOfSheets > 0){
-            if (costPerSheet >= 0.0001){
-                costPerSheetText.innerHTML = finalCost.bold() + "$" + costPerSheet.toFixed(4).toString();
-            }
-            else {
-                costPerSheetText.innerHTML = finalCost.bold() + "too low to calculate";
-            }
-        }
-}
-
-window.setInterval(CalculateCost, 200);
+  if (totalNumberOfSheets > 0) {
+    if (costPerSheet >= 0.0001) {
+      finalCostPrice.textContent = "$" + costPerSheet.toFixed(4).toString();
+    } else {
+      finalCostPrice.textContent = "too low to calculate";
+    }
+  }
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Toilet Paper Calculator
+
+This is a simple toilet paper calculator build using html/css and vanilla javascript. Simply input the cost, number of rolls per pack, and number of sheets per roll. The calculator will automatically update with each input.
